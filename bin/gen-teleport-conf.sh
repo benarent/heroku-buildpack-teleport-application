da4b866a6caa4baeb11d3dec5b0a99a86de3d8ab
Ben Arent <ben@goteleport.com>
2021-06-21 14:56:23 -0700

    Update buildpack

--- bin/gen-teleport-conf.sh
+++ bin/gen-teleport-conf.sh
@@ -4,29 +4,29 @@ TELEPORT_URL=asteroid-moon.teleport.sh:443
 TELEPORT_API=1be2b449659dc89419875b36a53e5757
 TELEPORT_HEROKU_URL=https://aap-jwt.herokuapp.com
 
-cat >> /app/vendor/teleport/teleport.yaml << EOF
-teleport:
-  data_dir: /app/vendor/teleport/
-  auth_token: ${TELEPORT_API}
-  auth_servers:
-  - ${TELEPORT_URL}
-  log:
-    output: stderr
-    severity: DEBUG
-auth_service:
-  enabled: no
-ssh_service:
-  enabled: no
-  labels:
-    cloud: heroku
-proxy_service:
-  enabled: no
-app_service:
-  enabled: yes
-  debug_app: true
-  apps:
-  - name: "herokutest3"
-    uri: "${TELEPORT_HEROKU_URL}"
+cat >> /app/vendor/teleport/teleport/teleport.yaml << EOF
+      teleport:
+      nodename: 581f063b-4921-4315-a121-21c8afceb9c8
+      data_dir: /app/vendor/teleport/
+      log:
+          output: stderr
+          severity: INFO
+      ca_pin: ""
+      auth_service:
+      enabled: "yes"
+      listen_addr: 0.0.0.0:3025
+      cluster_name: aap-jwt.herokuapp.com
+      ssh_service:
+      enabled: "yes"
+      proxy_service:
+      enabled: "yes"
+      listen_addr: 0.0.0.0:3023
+      web_listen_addr: :443
+      public_addr: aap-jwt.herokuapp.com:443
+      https_keypairs: []
+      acme:
+          enabled: "yes"
+          email: ben@goteleport.com
 EOF
 
 chmod go-rwx /app/vendor/teleport/*
