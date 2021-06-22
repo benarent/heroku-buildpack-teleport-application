daa7984e70f5dfafbedd8cfb0cf47d1955c8f483
Ben Arent <ben@goteleport.com>
2021-06-22 16:49:57 -0700

    Update gen-teleport-conf.sh

--- bin/gen-teleport-conf.sh
+++ bin/gen-teleport-conf.sh
@@ -6,27 +6,22 @@ TELEPORT_HEROKU_URL=https://aap-jwt.herokuapp.com
 
 cat >> /app/vendor/teleport/teleport.yaml << EOF
       teleport:
-        nodename: 581f063b-4921-4315-a121-21c8afceb9c8
+        auth_token: 1be2b449659dc89419875b36a53e5757
+        auth_servers:
+        - asteroid-moon.teleport.sh:443
         data_dir: /app/vendor/teleport/
         log:
             output: stderr
             severity: INFO
         ca_pin: ""
       auth_service:
-        enabled: "yes"
-        listen_addr: 0.0.0.0:3025
-        cluster_name: aap-jwt.herokuapp.com
+        enabled: "no"
       ssh_service:
         enabled: "yes"
       proxy_service:
         enabled: "yes"
-        listen_addr: 0.0.0.0:3023
-        web_listen_addr: :3080
-        public_addr: aap-jwt.herokuapp.com:3080
-        https_keypairs: []
-        acme:
-          enabled: "yes"
-          email: ben@goteleport.com
+        - name: "demo-app"
+        uri: "https://aap-jwt.herokuapp.com
 EOF
 
 chmod go-rwx /app/vendor/teleport/*
