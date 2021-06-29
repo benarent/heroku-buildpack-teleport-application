b7bb0483ff04a47221abb5a87d6eb4c8feac2906
Ben Arent <ben@goteleport.com>
2021-06-29 14:47:07 -0700

    Update teleport.sh

--- extra/teleport.sh
+++ extra/teleport.sh
@@ -6,26 +6,6 @@ export PATH=$PATH:$HOME/vendor/bin
 # set a default LANG if it does not exist in the environment
 export LANG=${LANG:-en_US.UTF-8}
 
-
-# Setup Locations
-main() {
-  echo "-----> RUN TELEPORT.SH" 
-  run-teleport "$@"
-}
-
-run-teleport() {
-  declare psmgr=/tmp/teleport-buildpack-wait
-  declare -A pids signals
-
-  config-gen
-
-  bash -c "~/vendor/teleport/teleport/teleport start --config=/app/vendor/teleport/teleport.yaml"
-
-}
-
-config-gen() {
-  # Generate config files
-  at config-gen-start
-  source ~/bin/gen-teleport-conf.sh
-  at config-gen-end
-}
+echo "-----> RUN TELEPORT.SH" 
+source ~/bin/gen-teleport-conf.sh
+bash -c "~/vendor/teleport/teleport/teleport start --config=/app/vendor/teleport/teleport.yaml"
