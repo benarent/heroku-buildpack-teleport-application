2b7a815dfa7b990956eed7e462fca352618520c0
Ben Arent <ben@goteleport.com>
2021-06-29 14:31:06 -0700

    Update teleport.sh

--- extra/teleport.sh
+++ extra/teleport.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# add vendor binaries to the path
+export PATH=$PATH:$HOME/vendor/bin
+
+# set a default LANG if it does not exist in the environment
+export LANG=${LANG:-en_US.UTF-8}
+
+
 # Setup Locations
 main() {
+  echo "-----> RUN TELEPORT.SH" 
   run-teleport "$@"
 }
 
